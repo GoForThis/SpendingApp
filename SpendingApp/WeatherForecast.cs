afb6168ec8dbd362f7a7a90e34fbdc29b84dd123
Jakub Sypek <jakubsypek23@gmail.com>
2022-06-10 21:18:48 +0200

    Initial commit

---
+++ SpendingApp/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace SpendingApp
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
