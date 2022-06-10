37bd3b366e2b8eab0421e02c7f07f655ecf92920
Jakub Sypek <jakubsypek23@gmail.com>
2022-06-10 23:52:12 +0200

    Adding Exceptions

---
+++ SpendingApp/Exceptions/BadRequestException.cs
@@ -0,0 +1,7 @@
+﻿namespace SpendingApp.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message) { }
+    }
+}
---
+++ SpendingApp/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace SpendingApp.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
