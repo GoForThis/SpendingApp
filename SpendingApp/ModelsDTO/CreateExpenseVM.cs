9294bffe69d0f68b785a160523d4b3133295457c
Jakub Sypek <jakubsypek23@gmail.com>
2022-06-10 23:31:10 +0200

    Add Expense dtos

---
+++ SpendingApp/ModelsDTO/CreateExpenseVM.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendingApp.ModelsDTO
+{
+    public class CreateExpenseVM
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Payment Method")]
+        [Required(ErrorMessage = "Payment Method is required")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Display(Name = "Amount")]
+        [Required(ErrorMessage = "Amount is required")]
+        public int Amount { get; set; }
+
+        [Display(Name = "Date")]
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Category")]
+        [Range(1, int.MaxValue, ErrorMessage = "Category is required")]
+        public int ExpenseCategoryId { get; set; }
+    }
+}
---
+++ SpendingApp/ModelsDTO/ExpenseDTO.cs
@@ -0,0 +1,14 @@
+﻿using SpendingApp.Entities;
+using SpendingApp.Entities.Enums;
+
+namespace SpendingApp.ModelsDTO
+{
+    public class ExpenseDTO
+    {
+        public string Name { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+    }
+}
