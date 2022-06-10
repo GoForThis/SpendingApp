a445a4d084f96788395d6a1691c7e09b4a015016
Jakub Sypek <jakubsypek23@gmail.com>
2022-06-10 23:21:15 +0200

    Add ExpenseCategory dtos

---
+++ SpendingApp/ModelsDTO/ExpenseCategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SpendingApp.ModelsDTO
+{
+    public class ExpenseCategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SpendingApp/ModelsDTO/ExpenseCategoryVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendingApp.ModelsDTO
+{
+    public class ExpenseCategoryVM
+    {
+        [Display(Name = "Category")]
+        [Required]
+        string Name { get; set; }
+    }
+}
