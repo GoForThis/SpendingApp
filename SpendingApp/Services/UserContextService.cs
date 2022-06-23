67719358a75889bb963c7de121604678c52376ed
Jakub Sypek <jakubsypek23@gmail.com>
2022-06-23 23:44:18 +0200

    Add UserContextService

---
+++ SpendingApp/Services/UserContextService.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+namespace SpendingApp.Services
+{
+    public interface IUserContextService
+    {
+        ClaimsPrincipal User { get; }
+        int? GetUserId { get; }
+    }
+
+    public class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public UserContextService(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public ClaimsPrincipal User => _accessor.HttpContext?.User;
+
+        public int? GetUserId => User is null ? null : (int?)int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+    }
+}
