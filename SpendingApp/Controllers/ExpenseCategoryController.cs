12ab08063d6551fc79ac79ca1b835451bf9d0b19
Jakub Sypek <jakubsypek23@gmail.com>
2022-06-25 09:17:15 +0200

    Add ExpenseCategoryController

---
+++ SpendingApp/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SpendingApp.ModelsDTO;
+using SpendingApp.Services;
+
+namespace SpendingApp.Controllers
+{
+    [Authorize]
+    [Route("api/category")]
+    [ApiController]
+    public class ExpenseCategoryController : ControllerBase
+    {
+        private readonly IExpenseCategoryService _expenseCategoryService;
+
+        public ExpenseCategoryController(IExpenseCategoryService expenseCategoryService)
+        {
+            _expenseCategoryService = expenseCategoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpenseCategoryVM>>> GetAll()
+        {
+            var categories = await _expenseCategoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExpenseCategoryVM>> GetById([FromRoute] int id)
+        {
+            var category = await _expenseCategoryService.GetById(id);
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ExpenseCategoryVM>> Create([FromBody] ExpenseCategoryVM category)
+        {
+            await _expenseCategoryService.Create(category);
+            return Created($"/api/categories", null);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update([FromRoute] int id, [FromBody] ExpenseCategoryVM category)
+        {
+            await _expenseCategoryService.Update(id, category);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] int id)
+        {
+            await _expenseCategoryService.Remove(id);
+            return NoContent();
+        }
+    }
+}
---
+++ SpendingApp/Controllers/ExpenseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpendingApp.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
